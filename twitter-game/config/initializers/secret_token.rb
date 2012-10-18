4a4ded01f37931d247f5a1fe6af3bc4fe2c8e970
Renae Bair <renaebair@gmail.com>
2012-10-18 19:20:44 -0400

    initial commit, rails app

---
+++ twitter-game/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterGame::Application.config.secret_token = 'd452f2c3db783ab879a1649cc44e27093ed034e69a6f288d17f5a411aff99d3afc19511e28c6b7218ef8b77b8c966829425ec72c4baac5eb3835841b9094f6b6'
